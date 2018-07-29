8498e3f0343b689376e48db8e0572d7e5245a85d
mmekhanov <mikhail.mekhanov@eurochem.ru>
2018-07-29 13:21:18 +0300

    Init repo

---
+++ colors_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestColor(t *testing.T) {
+	str := "i am green"
+
+	green := ColorOK(str)
+
+	assert.Contains(t, green, str)
+}
